88d3a7f7c2cb605b3d0c5cef59fa5676137ba79c
Marya2b <maryamamirkhani4@gmail.com>
2025-11-17 23:04:12 +0330

    Added static tags

--- cvsite/urls.py
+++ cvsite/urls.py
@@ -1,3 +1,11 @@
+from django.contrib import admin
+from django.urls import path,include 
+
+
+urlpatterns= [
+    path('admin/', admin.site.urls),
+    path('', include('resume.urls'))
+]
 from django.conf import settings
 from django.conf.urls.static import static
 
